34f3b5f7c7c5c802b113bbf17b2d93dfe22f088c
mtotodev05 <mtotodev05@gmail.com>
2017-06-09 22:09:26 +0300

    Main Service

--- src/main/java/com/brian/Api.java
+++ src/main/java/com/brian/Api.java
@@ -1,7 +1,29 @@
 package com.brian;
+import com.google.gson.Gson;
+import com.brian.model.blog;
+import com.brian.service.userService;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * Created by brian-kamau on 6/9/17.
  */
 public class Api {
+    public static userService userservice = new userService();
+
+    public static void main(String [] args){
+        System.out.println("Welcome To The World Of Application Programming Interfaces.");
+        final Gson gson = new Gson();
+        post("/add-post",(req,res)->{
+            res.type("application/json");
+            blog blog = gson.fromJson(res.body(), com.brian.model.blog.class);
+            return userservice.addPost(blog);
+        },gson::toJson);
+
+        get("/",(req,res)->{
+            res.type("application/json");
+            return userservice.getAllPosts();
+        },gson::toJson);
+    }
 }
