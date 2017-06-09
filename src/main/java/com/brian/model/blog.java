32aa5833dc6bd1eb58d4f8b11e5ed3253847b35f
mtotodev05 <mtotodev05@gmail.com>
2017-06-09 20:08:11 +0300

    Blog

--- src/main/java/com/brian/model/blog.java
+++ src/main/java/com/brian/model/blog.java
@@ -1,7 +1,31 @@
 package com.brian.model;
 
-/**
- * Created by brian-kamau on 6/9/17.
- */
+import com.sun.corba.se.spi.ior.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+
+@Entity
 public class blog {
+    private ObjectId id;
+    private String title;
+    private String description;
+    private String oth;
+
+
+    public blog(){
+        //Constructor.
+    }
+    public blog(String title,String description,String oth){
+        //Overloaded Constructor.
+        this.description=description;
+        this.title=title;
+        this.oth=oth;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 }
